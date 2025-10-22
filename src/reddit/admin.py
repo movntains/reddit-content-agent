b83b8f7fbdc5ae922215870a1f73364b24ccbca0
Rachel Opperman <oppermanre17@gmail.com>
2025-10-21 23:36:18 -0400

    feat(reddit): store communities

--- src/reddit/admin.py
+++ src/reddit/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import RedditPost
+from .models import RedditCommunity, RedditPost
 
 
 class RedditPostAdmin(admin.ModelAdmin):
@@ -8,4 +8,9 @@ class RedditPostAdmin(admin.ModelAdmin):
     list_filter = ["community_name"]
 
 
+class RedditCommunityAdmin(admin.ModelAdmin):
+    list_display = ["name", "subreddit_slug", "member_count"]
+
+
 admin.site.register(RedditPost, RedditPostAdmin)
+admin.site.register(RedditCommunity, RedditCommunityAdmin)
--- src/reddit/models.py
+++ src/reddit/models.py
@@ -23,3 +23,24 @@ class RedditPost(models.Model):
             return f"{self.url}"
 
         return f"{self.title}"
+
+
+class RedditCommunity(models.Model):
+    url = models.URLField(
+        db_index=True, help_text="The complete URL of the Reddit community"
+    )
+    name = models.TextField(
+        null=True, blank=True, help_text="Formatted name for Reddit"
+    )
+    subreddit_slug = models.CharField(
+        max_length=400,
+        null=True,
+        blank=True,
+        help_text="The slug of the subreddit, such as r/Python, r/web, or r/trending",
+    )
+    member_count = models.IntegerField(
+        null=True, blank=True, help_text="Current member count, if available"
+    )
+    active = models.BooleanField(default=True, help_text="Is this searchable?")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
